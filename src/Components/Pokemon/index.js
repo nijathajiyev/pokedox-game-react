4c68a59e0268cd69751f1a0a71b48eb46b1dd052
nijathajiyev <nijat.hajiyev7@gmail.com>
2022-06-07 15:42:25 +0400

    update

--- src/Components/Pokemon/index.js
+++ src/Components/Pokemon/index.js
@@ -2,21 +2,9 @@ import { Component } from "react";
 import { Card } from "../Card";
 
 export class Pokemon extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
   render() {
-    let { data, win } = this.props;
+    let { data } = this.props;
 
-    let title;
-    if (win) {
-      title = <h1>Winner</h1>;
-    } else {
-      title = <h1>Lost</h1>;
-    }
     return (
       <div>
         <div className="d-flex justify-content-center">
